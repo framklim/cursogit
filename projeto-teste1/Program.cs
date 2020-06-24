330432930cf5c28b55c9d6bb40ce571693a89063
framklim <framklim@gmail.com>
2020-06-23 21:18:30 -0300

    Acrescentado um bom dia!

--- projeto-teste1/Program.cs
+++ projeto-teste1/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste1
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
