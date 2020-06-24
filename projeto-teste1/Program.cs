a47154c34a4e7849ae26c1d3b80cbb5845d58a2b
framklim <framklim@gmail.com>
2020-06-23 21:15:43 -0300

    Projeto iniciado mostrando ola mundo!

---
+++ projeto-teste1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projeto_teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
